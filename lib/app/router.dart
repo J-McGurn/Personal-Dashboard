e6eda859d862b6835580858f23ca8e1db2834416
Jamie McGurn <152567001+J-McGurn@users.noreply.github.com>
2026-04-12 00:31:26 +0100

    Feat: Set up minimal project structure (#4)
    * feat: Set up minimal project structure
    
    * feat: Fixed CI Issue with structure
    
    * fix: Removed tests to be added back later when relevent
    
    * feat: replaced default test with placeholder test to pass CI

---
+++ lib/app/router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../features/dashboard/view/dashboard_page.dart';
+
+class AppRouter {
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const DashboardPage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Route not found')),
+          ),
+        );
+    }
+  }
+}
